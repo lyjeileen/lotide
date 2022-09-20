d529d75019d62ae03596e915fda04d593a2a95d3
lyjeileen <lyjeileen@gmail.com>
2022-09-20 03:07:07 +0000

    Add content in assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,25 @@
+const emoji = require("node-emoji");
+const smile = emoji.get("smile");
+const cry = emoji.get("cry");
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(
+      `${cry}${cry}${cry}` + "Assertion failed: " + actual + "!== " + expected
+    );
+  } else {
+    console.log(
+      `${smile}${smile}${smile}` +
+        "Assertion Passed: " +
+        actual +
+        " === " +
+        expected
+    );
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("a", "a");
+assertEqual(1, 3);
