5b8aa8cfa9d98915edf2d3fb9404f66545b218d9
lyjeileen <lyjeileen@gmail.com>
2022-09-20 03:41:45 +0000

    Add head.js

---
+++ head.js
@@ -0,0 +1,22 @@
+const emoji = require("node-emoji");
+const smile = emoji.get("smile");
+const cry = emoji.get("cry");
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(
+      `${cry}${cry}${cry}Assertion Failed: ${actual} !== ${expected}`
+    );
+  } else {
+    console.log(
+      `${smile}${smile}${smile}Assertion Passed: ${actual} === ${expected}`
+    );
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
