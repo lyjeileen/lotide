7d00f8649952c1828890ead516f31b87485c5c2d
lyjeileen <lyjeileen@gmail.com>
2022-09-21 16:15:24 +0000

    simplify function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,20 +2,23 @@ const emoji = require("node-emoji");
 const smile = emoji.get("smile");
 const cry = emoji.get("cry");
 
-const isArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    console.log(`${cry}${cry}${cry}${array1}!==${array2}`);
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        console.log(`${cry}${cry}${cry}${array1}!==${array2}`);
-        return;
-      }
-    }
-    console.log(`${smile}${smile}${smile}${array1}===${array2}`);
+    return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
-isArraysEqual([1, 2, 3], [1, 2, 3]);
-isArraysEqual([1, 2, 3], [3, 2, 1]);
-isArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-isArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+const assertArraysEqual = function(array1, array2) {
+  eqArrays(array1, array2) === false
+    ? console.log(`${cry}${cry}${cry}${array1}!==${array2}`)
+    : console.log(`${smile}${smile}${smile}${array1}===${array2}`);
+};
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
