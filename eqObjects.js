d154a2a4e7db2ce7151e0e9cce7e09f1cad889a8
lyjeileen <lyjeileen@gmail.com>
2022-09-22 02:28:16 +0000

    add eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,60 @@
+const emoji = require("node-emoji");
+const smile = emoji.get("smile");
+const cry = emoji.get("cry");
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(
+      `${cry}${cry}${cry}Assertion Failed: ${actual} !== ${expected}`
+    );
+  } else {
+    console.log(
+      `${smile}${smile}${smile}Assertion Passed: ${actual} === ${expected}`
+    );
+  }
+};
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (let key in object1) {
+      if (Array.isArray(object1[key])) {
+        if (eqArrays(object1[key], object2[key]) === false) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+const abWrong = { a: "1", b: "3" };
+assertEqual(eqObjects(ab, abWrong), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
