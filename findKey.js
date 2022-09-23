3342d132af6ec8870fcab45a955a342767e9d0dc
lyjeileen <lyjeileen@gmail.com>
2022-09-23 00:23:39 +0000

    add findKey.js

---
+++ findKey.js
@@ -0,0 +1,66 @@
+const emoji = require("node-emoji");
+const smile = emoji.get("smile");
+const cry = emoji.get("cry");
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(
+      `${cry}${cry}${cry}Assertion Failed: ${actual} !== ${expected}`
+    );
+  } else {
+    console.log(
+      `${smile}${smile}${smile}Assertion Passed: ${actual} === ${expected}`
+    );
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 5
+  ),
+  undefined
+);
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: {},
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "Ora"
+);
