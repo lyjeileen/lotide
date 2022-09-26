1080ebee523aa00b3aba3740ee81efe5804e0b26
lyjeileen <lyjeileen@gmail.com>
2022-09-26 22:33:05 +0000

    modulize assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,8 @@
-const emoji = require("node-emoji");
-const smile = emoji.get("smile");
-const cry = emoji.get("cry");
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = function (array1, array2) {
   eqArrays(array1, array2) === false
-    ? console.log(`${cry}${cry}${cry}${array1}!==${array2}`)
-    : console.log(`${smile}${smile}${smile}${array1}===${array2}`);
+    ? console.log(`🛑🛑🛑${array1}!==${array2}`)
+    : console.log(`✅✅✅ ${array1}===${array2}`);
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+module.exports = assertArraysEqual;
