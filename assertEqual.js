52568f8524c9e77955dc217daa38ddb3b38f6c0f
lyjeileen <lyjeileen@gmail.com>
2022-09-26 22:01:33 +0000

    use exports and require to refractor code

--- assertEqual.js
+++ assertEqual.js
@@ -1,21 +1,9 @@
-const emoji = require("node-emoji");
-const smile = emoji.get("smile");
-const cry = emoji.get("cry");
-
 const assertEqual = function (actual, expected) {
   if (actual !== expected) {
-    console.log(
-      `${cry}${cry}${cry}Assertion Failed: ${actual} !== ${expected}`
-    );
-  } else {
-    console.log(
-      `${smile}${smile}${smile}Assertion Passed: ${actual} === ${expected}`
-    );
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return;
   }
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("a", "a");
-assertEqual(1, 3);
+module.exports = assertEqual;
