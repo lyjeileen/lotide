7f6ce0ab896de05352a3e20c862978d0804d0700
lyjeileen <lyjeileen@gmail.com>
2022-09-26 22:20:17 +0000

    modulize eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,4 @@
-const emoji = require("node-emoji");
-const smile = emoji.get("smile");
-const cry = emoji.get("cry");
-
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(
-      `${cry}${cry}${cry}Assertion Failed: ${actual} !== ${expected}`
-    );
-  } else {
-    console.log(
-      `${smile}${smile}${smile}Assertion Passed: ${actual} === ${expected}`
-    );
-  }
-};
-
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -25,7 +9,5 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+module.exports = eqArrays;
