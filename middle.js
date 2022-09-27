d8bc79612fc0e37a2cf95929d9ed1057bce75f63
lyjeileen <lyjeileen@gmail.com>
2022-09-27 00:29:52 +0000

    fix error in middle.js

--- middle.js
+++ middle.js
@@ -1,13 +1,11 @@
 const middle = function (array) {
   const median = Math.round(array.length / 2);
-  console.log(median);
-  let result = [];
   if (array.length > 2) {
     if (array.length % 2 === 0) {
-      result = array.slice(median - 1, median + 1);
+      return array.slice(median - 1, median + 1);
     }
-    result = array.slice(median - 1, median);
+    return array.slice(median - 1, median);
   }
-  return result;
+  return [];
 };
 module.exports = middle;
